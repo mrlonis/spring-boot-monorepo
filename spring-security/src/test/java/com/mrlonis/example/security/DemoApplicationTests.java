6d5e608c27eab3085430281acda79ffe21a9281f
Matthew Lonis <mattlonis@gmail.com>
2025-05-16 22:30:25 -0400

    Updating the security app

--- spring-security/src/test/java/com/mrlonis/example/security/DemoApplicationTests.java
+++ spring-security/src/test/java/com/mrlonis/example/security/DemoApplicationTests.java
@@ -1,14 +1,24 @@
 package com.mrlonis.example.security;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.mrlonis.example.security.test.AbstractMockWebServerTests;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTests extends AbstractMockWebServerTests {
     @Test
     void contextLoads() {
         assertTrue(true);
+    }
+
+    @Test
+    void testActuator_healthIsOk() {
+        assertDoesNotThrow(() -> mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("UP")));
     }
 }
