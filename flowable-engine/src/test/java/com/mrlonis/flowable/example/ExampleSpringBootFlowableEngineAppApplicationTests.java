aa479dd8630bac8383444d85d254454b5fc03b28
Matthew Lonis <mattlonis@gmail.com>
2025-05-16 21:16:09 -0400

    Fixing flowable engine

--- flowable-engine/src/test/java/com/mrlonis/flowable/example/ExampleSpringBootFlowableEngineAppApplicationTests.java
+++ flowable-engine/src/test/java/com/mrlonis/flowable/example/ExampleSpringBootFlowableEngineAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mrlonis.flowable.example;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -8,5 +10,7 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class ExampleSpringBootFlowableEngineAppApplicationTests {
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        assertTrue(true);
+    }
 }
