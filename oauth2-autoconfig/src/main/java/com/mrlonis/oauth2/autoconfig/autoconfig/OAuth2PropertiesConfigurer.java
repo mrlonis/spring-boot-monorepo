446422cc0eb392838528ac8f5ba8544b19bf6a23
Matthew Lonis <mattlonis@gmail.com>
2025-05-27 22:17:56 -0400

    Fixing build issues

---
+++ oauth2-autoconfig/src/main/java/com/mrlonis/oauth2/autoconfig/autoconfig/OAuth2PropertiesConfigurer.java
@@ -0,0 +1,42 @@
+package com.mrlonis.oauth2.autoconfig.autoconfig;
+
+import com.mrlonis.oauth2.autoconfig.properties.OAuth2AutoConfigurationProperties;
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+
+@EnableConfigurationProperties(OAuth2AutoConfigurationProperties.class)
+@AllArgsConstructor
+@AutoConfiguration
+@ConditionalOnProperty(name = "oauth2.enabled", havingValue = "true")
+@Slf4j
+public class OAuth2PropertiesConfigurer {
+    private final OAuth2AutoConfigurationProperties properties;
+    private final ConfigurableEnvironment environment;
+
+    @PostConstruct
+    public void applyDefaults() {
+        log.info("Configuring OAuth2 configuration");
+        Map<String, Object> defaults = new HashMap<>();
+        defaults.put("oauth2.security.default-any-request-access", "authenticated");
+
+        if (Arrays.stream(environment.getActiveProfiles()).anyMatch(profile -> StringUtils.equals("local", profile))
+                && properties.getFederate().isEnabled()) {
+            defaults.put("spring.security.oauth2.resourceserver.jwt.issuer-uri", "http://localhost:9562");
+            if (!properties.getFederate().isOpaque()) {
+                defaults.put("oauth2.security.federate.issuer-uri", "http://localhost:9562");
+                defaults.put("oauth2.security.federate.jwk-set-uri", "http://localhost:9562/oauth2/jwks");
+            }
+        }
+        environment.getPropertySources().addFirst(new MapPropertySource("oauth2-autoconfig.defaults", defaults));
+    }
+}
