6b320928a126608b01159904b70704f046d8fc10
Matthew Lonis <mattlonis@gmail.com>
2026-03-25 13:26:35 -0400

    Adding spring-security-reactive

---
+++ spring-security-reactive/src/test/java/com/mrlonis/example/security/test/TestApplication.java
@@ -0,0 +1,10 @@
+package com.mrlonis.example.security.test;
+
+import com.mrlonis.example.security.SpringSecurityApplication;
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.from(SpringSecurityApplication::main).run(args);
+    }
+}
