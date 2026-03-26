a7025164e8c04a5130d13df9e8fde54dedcc080c
Matthew Lonis <mattlonis@gmail.com>
2026-03-26 08:56:31 -0400

    Adding spring-security-reactive-opaque

---
+++ spring-security-reactive-opaque/src/test/java/com/mrlonis/example/security/reactive/opaque/test/TestApplication.java
@@ -0,0 +1,10 @@
+package com.mrlonis.example.security.reactive.opaque.test;
+
+import com.mrlonis.example.security.reactive.opaque.SpringSecurityReactiveOpaqueApplication;
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.from(SpringSecurityReactiveOpaqueApplication::main).run(args);
+    }
+}
