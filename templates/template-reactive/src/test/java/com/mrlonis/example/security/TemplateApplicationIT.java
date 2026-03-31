f234f85315ccec64a493c23c471bcfe924afac47
Matthew Lonis <mattlonis@gmail.com>
2026-03-30 21:24:08 -0400

    Adding basic reactive template

---
+++ templates/template-reactive/src/test/java/com/mrlonis/example/security/TemplateApplicationIT.java
@@ -0,0 +1,26 @@
+package com.mrlonis.example.security;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.mrlonis.example.security.test.AbstractMockWebServerIT;
+import org.junit.jupiter.api.Test;
+
+class TemplateApplicationIT extends AbstractMockWebServerIT {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testActuator_healthIsOk() {
+        webTestClient
+                .get()
+                .uri("/actuator/health")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$.status")
+                .isEqualTo("UP");
+    }
+}
---
+++ templates/template-reactive/src/test/java/com/mrlonis/example/security/test/AbstractMockWebServerIT.java
@@ -0,0 +1,38 @@
+package com.mrlonis.example.security.reactive.test;
+
+import java.io.IOException;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webtestclient.autoconfigure.AutoConfigureWebTestClient;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@ActiveProfiles("test")
+public abstract class AbstractMockWebServerIT {
+    protected static MockWebServer mockWebServer;
+
+    @Autowired
+    protected WebTestClient webTestClient;
+
+    @BeforeAll
+    static void setupMockWebServer() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @DynamicPropertySource
+    static void dynamicProperties(org.springframework.test.context.DynamicPropertyRegistry registry) {
+        registry.add("MOCK_WEB_SERVER_PORT", () -> mockWebServer.getPort());
+    }
+
+    @AfterAll
+    static void teardownMockWebServer() throws IOException {
+        mockWebServer.shutdown();
+    }
+}
